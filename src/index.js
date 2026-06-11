86e7b539719d8f7874722db1517f94af2ba713d5
Vale Puentes <valentinaapuentes@gmail.com>
2026-06-10 20:36:50 -0400

    test

--- src/index.js
+++ src/index.js
@@ -7,9 +7,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const { Rol, Usuario } = require('./models');
+const dominiosPermitidos = [
+    'http://localhost:5173', // Desarrollo local
+    process.env.FRONTEND_URL // Dominio de producción en Vercel
+];
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || dominiosPermitidos.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+    credentials: true
+}));
 app.use(express.json());
 
 // --- Importación de Rutas ---
